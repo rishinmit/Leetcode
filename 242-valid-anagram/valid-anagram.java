45c4e925b9440beac61b75e386173f24329105f8
Rishi Raj <153723381+rishinmit@users.noreply.github.com>
2025-04-01 23:54:53 +0530

    Time: 709 ms (5.35%) | Memory: 50.1 MB (7.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         int a = t.length();
         int b = s.length();
-        int c =0;
-        if(b>a){
+        int c = 0;
+        if (b > a) {
             return false;
         }
         List<String> myL = new ArrayList<String>(Arrays.asList(s.split("")));
         char[] arr = t.toCharArray();
-        for(int i =0; i<a ; i++){
-            if(myL.contains(String.valueOf(arr[i]))){
+        for (int i = 0; i < a; i++) {
+            if (myL.contains(String.valueOf(arr[i]))) {
                 c++;
                 myL.remove(String.valueOf(arr[i]));
             }
         }
-        if(c==a){
+        if (c == a) {
             return true;
         }
         return false;
