c0d964e537a41bcb08875dd18615d2c9c1fc1c53
Rishi Raj <153723381+rishinmit@users.noreply.github.com>
2025-04-20 15:24:04 +0530

    [29] [Time Beats: 74.57%] [Memory Beats: 76.93%] - LeetPush

---
+++ Divide-Two-Integers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean sign = true;
+        if(divisor==dividend) return 1;
+        if(dividend == 1 && dividend > divisor) return divisor;
+        if((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)){
+            sign = false;
+        }
+        else if((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)){
+            sign = true;
+        }
+        long num = Math.abs((long) dividend);
+        long div = Math.abs((long) divisor);   
+        int ans = 0;
+
+        while (num >= div) {
+            int c = 0;
+            while (num >= (div << (c + 1))) {
+                c++;
+            }
+            ans += (1 << c);
+            num -= (div << c);
+        }
+
+        return sign ? ans : -ans;
+    }
+}
