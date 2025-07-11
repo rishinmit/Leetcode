b8fee465d7df9e1f7b81b40304a71780b54720cd
Rishi Raj <153723381+rishinmit@users.noreply.github.com>
2025-07-11 18:07:15 +0530

    [1] [Time Beats: 98.98%] [Memory Beats: 5.12%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       HashMap<Integer, Integer> hmap = new HashMap<>();
-       for(int i=0; i<nums.length; i++){
-        int comp = target - nums[i];
-        if(hmap.containsKey(comp)){
-            return new int[] {hmap.get(comp),i};
-        }
-        else{
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        int a =nums.length;
+        int[] indices = new int[2];
+        for(int i=0; i<a; i++){
+            int subbtract = (target - nums[i]);
+            if(hmap.containsKey(subbtract)){
+                indices[0] = i;
+                indices[1] = hmap.get(subbtract);
+                break;
+            }
             hmap.put(nums[i],i);
         }
-       }
-       return new int[]{};
+        return indices;
     }
 }
