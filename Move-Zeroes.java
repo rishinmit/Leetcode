7f65bc7921bf5a7c5fe9642d7865260208705dd2
Rishi Raj <153723381+rishinmit@users.noreply.github.com>
2025-10-17 12:41:37 +0530

    [283] [Time Beats: 78.89%] [Memory Beats: 79.99%] - LeetPush

--- Move-Zeroes.java
+++ Move-Zeroes.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] moveZeroes(int[] nums) {
-        int a = nums.length;
-        int l = 0;
-        int temp;
-        for(int r=0; r<a; r++){
-            if(nums[r] != 0){
-                temp = nums[r];
-                nums[r] = nums[l];
-                nums[l] = temp;
-                l++;
+        int left = 0;
+        for(int right=0; right<nums.length; right++){
+            if(nums[right]!=0){
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
         }
         return nums;
